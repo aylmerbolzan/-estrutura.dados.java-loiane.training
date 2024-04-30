c4bff7ca20f61b6861e99eac6d86a8c5e091cf11
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-04-30 00:51:11 -0300

    aula 03: adicionar elemento no final do vetor

---
+++ estruturadados-algoritmos-java/src/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package vetor.teste;
+
+import vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(2);
+        try {
+            vetor.adiciona("Elemento 01");
+            vetor.adiciona("Elemento 02");
+            vetor.adiciona("Elemento 03");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
