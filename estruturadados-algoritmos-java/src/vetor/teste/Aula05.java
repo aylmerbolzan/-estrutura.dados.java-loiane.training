f00e382400e7c550d122a72660558318c490cf86
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-05-02 22:03:44 -0300

    aula 05: obter elemento de uma posição

---
+++ estruturadados-algoritmos-java/src/vetor/teste/Aula05.java
@@ -0,0 +1,14 @@
+package vetor.teste;
+
+import vetor.Vetor;
+
+public class Aula05 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+        vetor.adiciona("Elemento 01");
+        vetor.adiciona("Elemento 02");
+        vetor.adiciona("Elemento 03");
+
+        System.out.println(vetor.busca(15));
+    }
+}
